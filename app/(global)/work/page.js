d2bafbb26e4d8621a5947831849e1ef17f7ea5a0
Zaliqa Rosli <zaliqarosli@gmail.com>
2025-04-15 00:55:25 -0400

    add links to cards

--- app/(global)/work/page.js
+++ app/(global)/work/page.js
@@ -3,13 +3,19 @@ import Card from '../../ui/components/card';
 export default function Page() {
   return (
     <main className='flex py-[3.75rem] px-[1.875rem] justify-evenly items-center content-center gap-[1.875rem] self-stretch flex-wrap'>
-      <Card projectTitle='mtl bloc list' srcImage='/mtl-bloc-list@2x.jpg' />
       <Card
-        projectTitle='ui design for loris'
+        name='mtl-bloc-list'
+        title='mtl bloc list'
+        srcImage='/mtl-bloc-list@2x.jpg'
+      />
+      <Card
+        name='ui-design-loris'
+        title='ui design for loris'
         srcImage='/ui-design-loris@2x.jpg'
       />
       <Card
-        projectTitle='patient dashboard'
+        name='patient-dashboard'
+        title='patient dashboard'
         srcImage='/patient-profile-page@2x.jpg'
       />
     </main>
--- app/ui/components/card.js
+++ app/ui/components/card.js
@@ -1,20 +1,30 @@
-import Image from 'next/image';
+'use client';
 
-export default function Card({ projectTitle, srcImage }) {
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Card({ name, title, srcImage }) {
+  const pathname = usePathname();
+
   return (
-    <div className='flex min-w-[23.125rem] max-w-[42.2rem] min-h-[21.75rem] max-h-[40rem] flex-col flex-[1_0_0] aspect-[185/174] shadow-md shadow-brown-shadow'>
+    <Link
+      key={name}
+      href={`${pathname}/${name}`}
+      className='flex min-w-[23.125rem] max-w-[42.2rem] min-h-[21.75rem] max-h-[40rem] flex-col flex-[1_0_0] aspect-[185/174] shadow-md shadow-brown-shadow'
+    >
       <div className='min-w-[21.875rem] min-h-[13.835rem] flex-[1_0_0] relative'>
         <Image
           src={srcImage}
           fill={true}
-          alt={`screenshot of ${projectTitle} project`}
+          alt={`screenshot of ${title} project`}
           style={{ objectFit: 'cover' }}
         />
       </div>
       <div className='flex p-[1.875rem] flex-col gap-[1.25rem]'>
         <strong>View case study</strong>
-        <h4 className='self-stretch'>{projectTitle}</h4>
+        <h4 className='self-stretch'>{title}</h4>
       </div>
-    </div>
+    </Link>
   );
 }
