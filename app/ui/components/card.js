085f45526fb0f29319896686255a4b5ee975d869
Zaliqa Rosli <zaliqarosli@gmail.com>
2025-04-14 21:01:06 -0400

    start work page

---
+++ app/ui/components/card.js
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+export default function Card({ projectTitle, srcImage }) {
+  return (
+    <div className='flex min-w-[23.125rem] max-w-[42.2rem] min-h-[21.75rem] max-h-[40rem] flex-col flex-[1_0_0] aspect-[185/174] shadow-md shadow-brown-shadow'>
+      <div className='min-w-[21.875rem] min-h-[13.835rem] flex-[1_0_0] bg-yellow-100 relative'>
+        <Image
+          src={srcImage}
+          fill={true}
+          alt={`screenshot of ${projectTitle} project`}
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+      <div className='flex p-[1.875rem] flex-col gap-[1.25rem] bg-purple-300'>
+        <strong>View case study</strong>
+        <h4 className='self-stretch'>{projectTitle}</h4>
+      </div>
+    </div>
+  );
+}
--- app/work/page.js
+++ app/work/page.js
@@ -1,3 +1,17 @@
+import Card from './../ui/components/card';
+
 export default function Page() {
-  return <p>Work Page</p>;
+  return (
+    <main className='flex py-[3.75rem] px-[1.875rem] justify-evenly items-center content-center gap-[1.875rem] self-stretch flex-wrap'>
+      <Card projectTitle='mtl bloc list' srcImage='/mtl-bloc-list@2x.jpg' />
+      <Card
+        projectTitle='ui design for loris'
+        srcImage='/ui-design-loris@2x.jpg'
+      />
+      <Card
+        projectTitle='patient dashboard'
+        srcImage='/patient-profile-page@2x.jpg'
+      />
+    </main>
+  );
 }
