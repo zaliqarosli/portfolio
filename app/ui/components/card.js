4b4b134f1205bdfd3056460296c8600f1f7d2a37
Zaliqa Rosli <zaliqarosli@gmail.com>
2025-04-14 22:51:15 -0400

    rm temp bg colours

--- app/ui/components/card.js
+++ app/ui/components/card.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Card({ projectTitle, srcImage }) {
   return (
     <div className='flex min-w-[23.125rem] max-w-[42.2rem] min-h-[21.75rem] max-h-[40rem] flex-col flex-[1_0_0] aspect-[185/174] shadow-md shadow-brown-shadow'>
-      <div className='min-w-[21.875rem] min-h-[13.835rem] flex-[1_0_0] bg-yellow-100 relative'>
+      <div className='min-w-[21.875rem] min-h-[13.835rem] flex-[1_0_0] relative'>
         <Image
           src={srcImage}
           fill={true}
@@ -11,7 +11,7 @@ export default function Card({ projectTitle, srcImage }) {
           style={{ objectFit: 'cover' }}
         />
       </div>
-      <div className='flex p-[1.875rem] flex-col gap-[1.25rem] bg-purple-300'>
+      <div className='flex p-[1.875rem] flex-col gap-[1.25rem]'>
         <strong>View case study</strong>
         <h4 className='self-stretch'>{projectTitle}</h4>
       </div>
