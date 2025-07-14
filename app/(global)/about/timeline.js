b41ac1468a5e7125e7ea11ffe7d60e4168b287cb
Zaliqa Rosli <zaliqarosli@gmail.com>
2025-07-14 18:29:39 -0400

    fix connectors

--- app/(global)/about/timeline.js
+++ app/(global)/about/timeline.js
@@ -3,6 +3,7 @@ import InfoConnector from '@/app/ui/components/info-connector';
 
 export default function Timeline() {
   let currentYear = '';
+
   const timeline = myJourney.map((entry, i) => {
     const location = entry.location || '-';
     const description = entry.description || '-';
@@ -10,6 +11,7 @@ export default function Timeline() {
     // create title subheaders
     const subheaders = [];
     entry.roles.forEach((role, j) => {
+      let years = [];
       let titles = [];
       let connectors = [];
 
@@ -20,59 +22,115 @@ export default function Timeline() {
         currentYear = role.year;
       }
 
-      if (j === 0) {
+      // Check if add on title
+      let titleAddOn = false;
+
+      if (j === 0 || j > 1) {
+        let connectorRow = [];
         // create subheader with year and title containers
         // insert connector group and title group into title container
         // insert year text into year container if its not already been displayed
         // push connector dash to connector group if display year is true
         // push role.title to title group
-        titles.push(<h6>{role.title}</h6>);
-        // if roles.length > 1, push double info connector to connector group and roles[1].title to title group
-        if (entry.roles.length > 1) {
-          connectors.push(<InfoConnector key={i + j} type='double' />);
-          titles.push(<h6>{entry.roles[1].title}</h6>);
+        if (displayYear) {
+          years.push(<p key={`year-${i}-${j}`}>{role.year}</p>);
+          connectorRow.push(
+            <div
+              id={`connector-year-${i}-${j}-first`}
+              className='flex w-[1.25rem] flex-col items-end border-b border-solid border-midgray'
+            ></div>
+          );
+        } else {
+          connectorRow.push(
+            <div
+              id={`connector-year-${i}-${j}-first`}
+              className='flex w-[1.25rem] flex-col items-end'
+            ></div>
+          );
         }
-      } else if (j === 1) {
+        titles.push(<h6 key={`title-${i}-${j}`}>{role.title}</h6>);
+        connectors.push(
+          <div className='flex' id={`connector-row-${i}-${j}-first`}>
+            {connectorRow}
+          </div>
+        );
+
+        // if roles.length > 1, push double info connector to connector group and roles[1].title to title group
+        connectorRow = [];
+        if (j === 0 && entry.roles.length > 1) {
+          // Append second role entry to the first
+          const secondRole = entry.roles[1];
+          // Add year and connector to year container
+          if (secondRole.year != currentYear) {
+            years.push(<p key={`year-${i}-${j + 1}`}>{secondRole.year}</p>);
+            connectorRow.push(
+              <div
+                id={`connector-year-${i}-${j + 1}-second`}
+                className='flex w-[1.25rem] flex-col items-end border-b border-solid border-midgray'
+              ></div>
+            );
+          } else {
+            connectorRow.push(
+              <div
+                id={`connector-year-${i}-${j + 1}-second`}
+                className='flex w-[1.25rem] flex-col items-end'
+              ></div>
+            );
+          }
+          // Add title and info connector
+          titles.push(<h6 key={`title-${i}-${j + 1}`}>{secondRole.title}</h6>);
+          connectorRow.push(<InfoConnector key={i + j} type='double' />);
+          connectors.push(
+            <div className='flex' id={`connector-row-${i}-${j}-first`}>
+              {connectorRow}
+            </div>
+          );
+        } else if (j > 1) {
+          // else create new subheader with year and title containers
+          // insert connector group and title group into title container
+          // insert year text into year container if its not already been displayed
+          // push connector dash to connector group is display year is true
+          // push role.title to title group
+          titleAddOn = true;
+          // push single info connection to connector group
+          connectorRow.push(<InfoConnector key={i + j} />);
+          connectors.push(
+            <div className='flex' id={`connector-row-${i}-${j}-first`}>
+              {connectorRow}
+            </div>
+          );
+        }
+      } else {
         // else if j === 1, continue
         return;
-      } else {
-        // else create new subheader with year and title containers
-        // insert connector group and title group into title container
-        // insert year text into year container if its not already been displayed
-        // push connector dash to connector group is display year is true
-        // push role.title to title group
-        titles.push(<h6>{role.title}</h6>);
-        // push single info connection to connector group
-        connectors.push(<InfoConnector key={i + j} />);
       }
 
       subheaders.push(
         <div
+          key={`subheader-${i}-${j}`}
           id={`subheader-${i}-${j}`}
           className='flex items-start self-stretch'
         >
           <div
             id={`year-container-${i}-${j}`}
-            className='flex w-[4.375rem] pt-[0.8125rem] pr-[0.625rem] justify-end items-center self-stretch'
+            className={
+              'flex w-[4.375rem] pr-[0.625rem] justify-end items-center self-stretch' +
+              (titleAddOn ? ' pt-[0.8125rem]' : ' pt-[3.625rem]')
+            }
           >
-            {displayYear && <p>{currentYear}</p>}
+            {years}
           </div>
           <div
             id={`title-container-${i}-${j}`}
-            className='flex pt-[0.9375rem] items-center flex-[1_0_0] self-stretch border-l border-solid border-midgray'
+            className={
+              'flex items-center gap-[0.625rem] flex-[1_0_0] self-stretch border-l border-solid border-midgray' +
+              (titleAddOn ? ' pt-[0.9375rem]' : ' pt-[3.75rem]')
+            }
           >
-            <div
-              id={`connector-group-${i}-${j}`}
-              className='flex w-[1.25rem] pb-[0.125rem] flex-col items-end'
-            >
-              {displayYear && (
-                <div className='pt-[0.375rem] border-b border-solid border-midgray'></div>
-              )}
-              {connectors}
-            </div>
+            <div id={`connector-group-${i}-${j}`}>{connectors}</div>
             <div
               id={`title-group-${i}-${j}`}
-              className='flex flex-col justify-center items-center gap-[0.9375rem]'
+              className='flex flex-col gap-[0.9375rem]'
             >
               {titles}
             </div>
@@ -100,6 +158,7 @@ export default function Timeline() {
 
     return (
       <div
+        key={`experience-${i}`}
         id={`experience-${i}`}
         className='flex flex-col items-start self-stretch'
       >
